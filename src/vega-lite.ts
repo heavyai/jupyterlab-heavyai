4508631642967308fe4da16502719533de003322
Saul Shanabrook <s.shanabrook@gmail.com>
2018-05-08 23:09:32 -0400

    Add more vega lite workarounds

--- src/vega-lite.ts
+++ src/vega-lite.ts
@@ -4,6 +4,8 @@ import { compile } from "vega-lite";
  * Compiles a Mapd vega lite spec to a Mapd vega spec.
  * 
  * The vega lite spec should have an `sql` field in it's data instead of a `values` field.
+ * 
+ * The vega spec has a bunch of differences between mapd and the original spec.
  * @param spec 
  */
 export function compileToVega(vlSpec: any): any {
@@ -11,13 +13,42 @@ export function compileToVega(vlSpec: any): any {
     delete vlSpec.data.sql;
     vlSpec.data.name = 'mapd_data' 
 
-
     const vSpec = compile(vlSpec, {config: {invalidValues: null}}).spec;
 
     // manually remove transformation from vega spec
     // until https://github.com/vega/vega-lite/issues/3665 is merged
     vSpec.data[0].name = vSpec.data.pop().name;
     vSpec.data[0].sql = sql;
-    console.log(vSpec);
+    
+    for (const mark of vSpec.marks) {
+        // mapd uses mark.properties instead of mark.encode.update
+        const properties = mark.encode.update;
+        mark.properties = properties;
+        delete mark.encode;
+
+        // mapd has an opacity value instead of an object
+        if (properties.opacity) {
+            properties.opacity = properties.opacity.value;
+        }
+
+        // mapd has a fillColor instead of a fill object
+        if (properties.fill) {
+            properties.fillColor = properties.fill.value;
+            delete properties.fill;
+        }
+
+        //mapd has a shape string instead of object
+        if (properties.shape) {
+            properties.shape = properties.shape.value;
+        }
+
+        // the width and height are required in mapd for symbols,
+        // unlike in vega where they are optional
+        if (mark.type === 'symbol') {
+            properties.width = 1;
+            properties.height = 1;
+        }
+    }
+
     return vSpec;
 }
