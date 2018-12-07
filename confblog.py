62b4075981e908e30e48cc9f75c5f31fb24ba7f0
Tony Fast <tony.fast@gmail.com>
2018-12-07 12:32:30 -0500

    Update confblog.py

--- confblog.py
+++ confblog.py
@@ -17,7 +17,7 @@ import time
 
 # Data about this site
 BLOG_AUTHOR = "Quansight"  # (translatable)
-BLOG_TITLE = "quansight"  # (translatable)
+BLOG_TITLE = "jupyterlab-omnisci research and documentation"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
 SITE_URL = "https://jupyterlab-omnisci.readthedocs.io/en/latest/blog"
