a2fdf818c0393c7fea94b9a018db6cdbb6e20b80
Saul Shanabrook <s.shanabrook@gmail.com>
2019-04-08 12:22:47 -0400

    Have a first go at adding pipeline

---
+++ jupyterlab_omnisci/altair_new.py
@@ -0,0 +1,77 @@
+import typing
+
+import ibis
+import altair
+import ipykernel.comm
+import mypy_extensions
+
+__all__: typing.List[str] = []
+
+
+# We need to persist this global mapping because the ibis hash
+# is stored in the Vega Lite spec and is sent back whenever we need to compute
+# new data.
+HASH_TO_IBIS: typing.Dict[int, ibis.Expr] = {}
+
+# New Vega Lite renderer mimetype which can process ibis expressions in names
+MIMETYPE = "application/vnd.vegalite.v3+json; ibis=true"
+
+NAME_PREFIX = 'ibis-'
+
+# Comm opened by client to send expression and transforms
+COMM_ID = 'vega-ibis'
+COMM = ipykernel.comm.Comm(target_name=COMM_ID, data=None)
+
+ipykernel.comm.CommManager.
+altair.data_transformers.register("vega-ibis", vega_ibis_transformation)
+
+
+def vega_ibis_transformation(data):
+    """
+    Saves the Ibis expression in `HASH_TO_IBIS` so we can access it later and
+    replaces it with named data that has the hash of the expression. 
+    """
+    # Requires dataframe to be pandas DF
+    assert isinstance(data, pandas.DataFrame)
+    expr = data.ibis
+    # Requires it to have ibis expression attached
+    assert isinstance(expr, ibis.Expr)
+
+    expr_hash = hash(expr)
+    HASH_TO_IBIS[expr_hash] = expr
+
+    return {"name": f"{NAME_PREFIX}{expr_hash}"}
+
+
+altair.renderers.register("vega-ibis", vega_ibis_renderer)
+
+
+def vega_ibis_renderer(spec):
+    """
+    Exports the spec with a new mimetype that will trigger the ibis-vega pipeline.
+    """
+    return {MIMETYPE: spec}
+
+
+
+
+
+
+CommData = mypy_extensions.TypedDict('CommData', {
+    "data": typing.Dict[str, object],
+    "expr_hash": int,
+    "transform": typing.List[typing.Dict]
+})
+
+
+
+@COMM.on_msg
+def comm_on_msg(msg) -> None:
+    on_comm_data(msg["content"]["data"])
+
+def on_comm_data(data: CommData) -> None:
+    """
+    Should process the vega data and send a message back with the updated data.
+    """
+
+    print(data)
---
+++ src/vega-ibis.ts
@@ -0,0 +1,94 @@
+import { JupyterLab, JupyterLabPlugin } from '@jupyterlab/application';
+import { DocumentRegistry } from '@jupyterlab/docregistry';
+import { INotebookModel, NotebookPanel } from '@jupyterlab/notebook';
+import { IRenderMimeRegistry, IRenderMime } from '@jupyterlab/rendermime';
+import { Kernel, KernelMessage } from '@jupyterlab/services';
+import { DisposableDelegate, IDisposable } from '@phosphor/disposable';
+import { Widget } from '@phosphor/widgets';
+
+import vegaEmbed from 'vega-embed';
+import * as vegaLite from 'vega-lite';
+import * as vega from 'vega';
+import ibisTransform from './ibis-transform';
+const PLUGIN_ID = 'jupyterlab-omnisci:vega-ibis';
+
+const plugin: JupyterLabPlugin<void> = {
+  activate,
+  id: PLUGIN_ID,
+  requires: [IRenderMimeRegistry],
+  autoStart: true
+};
+export default plugin;
+
+const COMM_ID = 'vega-ibis';
+
+const MIMETYPE = 'application/vnd.vegalite.v3+json; ibis=true';
+
+const NAME_PREFIX = 'ibis-';
+
+const TRANSFORM = 'ibis';
+
+
+function commTarget(comm: Kernel.IComm, msg: KernelMessage.ICommOpenMsg) {
+    ibisTransform.conn(comm);
+    comm.
+}
+
+function createNew(
+  nb: NotebookPanel,
+  context: DocumentRegistry.IContext<INotebookModel>
+): IDisposable {
+  context.session.kernelChanged.connect((_, { newValue, oldValue }) => {
+    if (newValue) {
+      newValue.registerCommTarget(COMM_ID, commTarget);
+    }
+  });
+  return new DisposableDelegate(() => {});
+}
+
+function activate(app: JupyterLab, rendermime: IRenderMimeRegistry) {
+  app.docRegistry.addWidgetExtension('Notebook', { createNew });
+
+  rendermime.addFactory({
+    safe: true,
+    defaultRank: 50,
+    mimeTypes: [MIMETYPE],
+    createRenderer: options => new Renderer()
+  });
+}
+
+class Renderer extends Widget implements IRenderMime.IRenderer {
+  async renderModel(model: IRenderMime.IMimeModel): Promise<void> {
+    const vlSpec: vegaLite.TopLevelSpec = model.data[MIMETYPE] as any;
+
+    const { spec } = vegaLite.compile(vlSpec);
+
+    transformVegaSpec(spec);
+
+    await vegaEmbed(this.node, spec, {
+      actions: true,
+      defaultStyle: true,
+      mode: 'vega'
+    });
+  }
+}
+
+/**
+ * Moves all transforms into a custom transform inside a ibis transform
+ */
+function transformVegaSpec(vgSpec: vega.Spec): void {
+  for (const data of vgSpec.data) {
+    if (!data.name.startsWith(NAME_PREFIX)) {
+      continue;
+    }
+    const hash = data.name.substring(NAME_PREFIX.length);
+    const transform = data.transform;
+    data.transform = [
+      // disable type checking because vega was not compiled with this transform
+      { type: TRANSFORM, hash, transform } as any
+    ];
+  }
+}
+
+
+(vega as any).transforms[TRANSFORM] = ibisTransform;
