9cd50fc966c628179afc3b3750cabf84245ed395
Ian Rose <ian.r.rose@gmail.com>
2019-05-03 12:18:53 -0700

    Pass evaluated ibis expression to the frontend.

--- jupyterlab_omnisci/vega_ibis.py
+++ jupyterlab_omnisci/vega_ibis.py
@@ -1,12 +1,13 @@
 import typing
 import copy
 import random
+import warnings
 
 import ibis
 import altair
 import pandas
 from ipykernel.comm import Comm
-import mypy_extensions
+from IPython import get_ipython
 
 __all__: typing.List[str] = []
 
@@ -30,37 +31,6 @@ EMPTY_VEGA = {
   "legends": [],
   "marks": []
 }
-
-def vega_ibis_transformer(data):
-    """
-    Saves the Ibis expression in `HASH_TO_IBIS` so we can access it later and
-    replaces it with named data that has the hash of the expression. 
-    """
-    # Requires it to have ibis expression attached
-    assert isinstance(data, ibis.Expr)
-
-    ibis_expr = data
-
-    expr_hash = hash(data)
-    HASH_TO_IBIS[expr_hash] = data
-
-    return {"name": f"{NAME_PREFIX}{expr_hash}"}
-
-
-def vega_ibis_renderer(spec):
-    """
-    Exports the spec with a new mimetype that will trigger the ibis-vega pipeline.
-    """
-    compile_comm = Comm(target_name='jupyterlab-omnisci:vega-compiler', data=spec)
-    display_id = display({ 'text/plain': '' }, display_id = True, raw=True) 
-
-    @compile_comm.on_msg
-    def _recv(msg):
-        display_id.update({MIMETYPE: msg['content', 'data']}, raw=True)
-
-
-    return { MIMETYPE: '' }
-
 
 def empty(expr: ibis.Expr) -> pandas.DataFrame:
     """
@@ -90,10 +60,13 @@ def monkeypatch_altair():
         return original_chart_init(self, data=data, *args, **kwargs)
 
     def ipython_display(self):
-        spec = self.to_dict()
-        _add_target()
-        compile_comm = Comm(target_name='jupyterlab-omnisci:vega-compiler', data=spec)
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            spec = self.to_dict()
         d = display({ MIMETYPE: EMPTY_VEGA }, raw=True, display_id=True)
+        _add_target(self.data.ibis)
+
+        compile_comm = Comm(target_name='jupyterlab-omnisci:vega-compiler', data=spec)
 
         @compile_comm.on_msg
         def _recv(msg):
@@ -119,24 +92,13 @@ def _transform(spec):
     new['data'][0]['values'] = []
     return new
 
-def _add_target():
+def _add_target(expr: ibis.Expr):
     def target_func(comm, msg):
         @comm.on_msg
         def _recv(msg):
-            # Use msg['content']['data'] for the data in the message
+            data = expr.execute()
+            comm.send(data.to_dict(orient='records'))
 
-            # Send data to the frontend
-            comm.send([{'a': 'A', 'b': random.randint(0,100)},
-                {'a': 'B', 'b': random.randint(0,100)},
-                {'a': 'C', 'b': random.randint(0,100)},
-                {'a': 'D', 'b': random.randint(0,100)},
-                {'a': 'E', 'b': random.randint(0,100)},
-                {'a': 'F', 'b': random.randint(0,100)},
-                {'a': 'G', 'b': random.randint(0,100)},
-                {'a': 'H', 'b': random.randint(0,100)},
-                {'a': 'I', 'b': random.randint(0,100)}])
     get_ipython().kernel.comm_manager.register_target('queryibis', target_func)
 
-altair.renderers.register("vega-ibis", vega_ibis_renderer)
-altair.data_transformers.register("vega-ibis", vega_ibis_transformer)
 monkeypatch_altair()
