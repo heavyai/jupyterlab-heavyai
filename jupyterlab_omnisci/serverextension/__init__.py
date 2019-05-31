55d1d26ee5dc619f54b5f8b1abbe6c300f9003d3
Ian Rose <ian.r.rose@gmail.com>
2019-05-30 19:17:51 -0700

    Begin adding session handler.

--- jupyterlab_omnisci/serverextension/__init__.py
+++ jupyterlab_omnisci/serverextension/__init__.py
@@ -2,7 +2,7 @@ from notebook.utils import url_path_join
 
 from jupyterlab_server import LabConfig
 
-from .handlers import OmniSciHandler
+from .handlers import OmniSciWorkspaceHandler, OmniSciSessionHandler
 
 def _jupyter_server_extension_paths():
     return [{"module": "jupyterlab_omnisci"}]
@@ -21,12 +21,16 @@ def load_jupyter_server_extension(nb_server_app):
     lab_path = url_path_join(base_url, lab_config.page_url)
 
     omnisci_endpoint = url_path_join(lab_path, "omnisci/workspace")
-    print(omnisci_endpoint)
+    omnisci_session_endpoint = url_path_join(lab_path, "omnisci/session")
     handlers = [
         (
             omnisci_endpoint,
-            OmniSciHandler,
+            OmniSciWorkspaceHandler,
             {'lab_config': nb_server_app.config},
+        ),
+        (
+            omnisci_session_endpoint,
+            OmniSciSessionHandler,
         ),
     ]
     web_app.add_handlers(".*$", handlers)
--- jupyterlab_omnisci/serverextension/handlers.py
+++ jupyterlab_omnisci/serverextension/handlers.py
@@ -1,6 +1,11 @@
-from jupyterlab_server import LabConfig, LabHandler
+import os
 
-class OmniSciHandler(LabHandler):
+from tornado import web
+
+from jupyterlab_server import LabConfig, LabHandler
+from notebook.base.handlers import APIHandler
+
+class OmniSciWorkspaceHandler(LabHandler):
     def initialize(self, lab_config):
         lab_config = LabConfig(config=lab_config)
         lab_config.templates_dir = '/home/ian/anaconda3/envs/omnisci/share/jupyter/lab/static'
@@ -9,3 +14,17 @@ class OmniSciHandler(LabHandler):
 
     """A handler that just delegates to the LabHandler"""
     pass
+
+class OmniSciSessionHandler(APIHandler):
+    @web.authenticated
+    def get(self):
+        data = {
+            'session': 'abc123',
+            'connection': {
+                'host': 'most',
+                'port': 3,
+                'protocol': 'ghost'
+            }
+        }
+        self.set_status(200)
+        self.finish(data)
