ad1b41806d63902bf98313200f318ae2743409c5
Saul Shanabrook <s.shanabrook@gmail.com>
2019-08-08 12:06:41 -0400

    Extract transforms before compiling to vega
    Fixes https://github.com/Quansight/jupyterlab-omnisci/issues/77 in the
    process, since now the axis sorting is moved to a transform

--- src/vega-compiler.ts
+++ src/vega-compiler.ts
@@ -1,4 +1,4 @@
-import { TopLevelSpec, compile } from 'vega-lite';
+import { TopLevelSpec, compile, extractTransforms } from 'vega-lite';
 
 import { Kernel } from '@jupyterlab/services';
 
@@ -15,7 +15,7 @@ export async function compileSpec(
   vlSpec: TopLevelSpec
 ): Promise<object> {
   // Compile vega-lite to vega
-  const vSpec = compile(vlSpec).spec;
+  const vSpec = compile(extractTransforms(vlSpec as any, {}) as any).spec;
 
   // Change vega transforms to ibis transforms on the server side.
   const transformedSpecPromise = new PromiseDelegate<any>();
