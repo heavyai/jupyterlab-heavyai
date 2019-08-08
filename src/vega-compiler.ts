599d38dbf001f6cf465d40dea4077f2b66a78810
Saul Shanabrook <s.shanabrook@gmail.com>
2019-08-08 15:26:47 -0400

    Temporarily disable extract transform to test demo

--- src/vega-compiler.ts
+++ src/vega-compiler.ts
@@ -1,8 +1,7 @@
-import { TopLevelSpec, compile, extractTransforms } from 'vega-lite';
-
 import { Kernel } from '@jupyterlab/services';
-
 import { PromiseDelegate } from '@phosphor/coreutils';
+import { compile, TopLevelSpec } from 'vega-lite';
+import { initConfig } from 'vega-lite/build/src/config';
 
 const PLUGIN_ID = 'jupyterlab-omnisci:vega-compiler';
 
@@ -14,8 +13,11 @@ export async function compileSpec(
   kernel: Kernel.IKernelConnection,
   vlSpec: TopLevelSpec
 ): Promise<object> {
-  // Compile vega-lite to vega
-  const vSpec = compile(extractTransforms(vlSpec as any, {}) as any).spec;
+  // uses same logic as
+  // https://github.com/vega/vega-lite/blob/a4ee4ec393d86b611f95486ad2e1902bfa3ed0cf/test/transformextract.test.ts#L133
+  const config = initConfig(vlSpec.config || {});
+  const extractSpec = vlSpec;
+  const vSpec = compile(extractSpec, { config }).spec;
 
   // Change vega transforms to ibis transforms on the server side.
   const transformedSpecPromise = new PromiseDelegate<any>();
