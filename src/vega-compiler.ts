a0450fb9b55d6b47fed832b11767a53d6df7a6c0
Saul Shanabrook <s.shanabrook@gmail.com>
2019-08-10 06:10:38 -0400

    Run extract transform on specs

--- src/vega-compiler.ts
+++ src/vega-compiler.ts
@@ -1,6 +1,6 @@
 import { Kernel } from '@jupyterlab/services';
 import { PromiseDelegate } from '@phosphor/coreutils';
-import { compile, TopLevelSpec } from 'vega-lite';
+import { compile, TopLevelSpec, extractTransforms, normalize } from 'vega-lite';
 import { initConfig } from 'vega-lite/build/src/config';
 
 const PLUGIN_ID = 'jupyterlab-omnisci:vega-compiler';
@@ -15,9 +15,16 @@ export async function compileSpec(
 ): Promise<object> {
   // uses same logic as
   // https://github.com/vega/vega-lite/blob/a4ee4ec393d86b611f95486ad2e1902bfa3ed0cf/test/transformextract.test.ts#L133
+
   const config = initConfig(vlSpec.config || {});
-  const extractSpec = vlSpec;
-  const vSpec = compile(extractSpec, { config }).spec;
+  console.log('Initial VegaLite Spec', vlSpec);
+  const extractSpec = extractTransforms(
+    normalize(vlSpec as any, config),
+    config
+  );
+  console.log('Extracted VegaLite Spec', extractSpec);
+  const vSpec = compile(extractSpec as any, { config }).spec;
+  console.log('Initial Vega Spec', vSpec);
 
   // Change vega transforms to ibis transforms on the server side.
   const transformedSpecPromise = new PromiseDelegate<any>();
