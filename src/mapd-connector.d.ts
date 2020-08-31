657d1c16f8904e69f40bc4190b24d77a79eb5410
saulshanabrook <saulshanabrook@users.noreply.github.com>
2020-08-31 17:08:21 +0000

    deploy: f3878d7a41bee289291edbfe732d142b0096c5ce

--- src/mapd-connector.d.ts
+++
@@ -1,1 +0,0 @@
-declare module 'mapd-connector/dist/browser-connector.js';
--- src/plugins.ts
+++
@@ -1,5 +0,0 @@
-import plugins from './extension';
-
-import vegaLitePlugin from './extract-vega-lite';
-
-export default [...plugins, vegaLitePlugin];
